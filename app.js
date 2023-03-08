c38834065b7b408b0d03f5ac4086b84c37b0d555
LinZH-1995 <lin84610@gmail.com>
2023-03-08 20:35:20 +0800

    add initial files

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'hbs')
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`Now Server is working on localhost:${port} !`)
