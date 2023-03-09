4d0545ed5580729d94795b8db47965e649a966cd
LinZH-1995 <lin84610@gmail.com>
2023-03-09 14:52:12 +0800

    set POST route

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const port = 3000
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  res.render('index')
+  console.log(req.body)
 })
 
 app.listen(port, () => {
